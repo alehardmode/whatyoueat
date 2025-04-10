dc9820160e748cebe4808cee8e7c7210b0be79cc
Alejandro <alejandrozamorabarrera@gmail.com>
2025-04-10 14:13:02 +0000

    feat: add Profile model tests in Profile.test.js and update Jest configuration to specify root directory and test match pattern, enhancing test coverage and organization

--- config/testing/jest.config.js
+++ config/testing/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  rootDir: '../../',
   testEnvironment: 'node', // Ambiente de ejecución para las pruebas (Node.js)
   verbose: true, // Mostrar detalles de cada prueba ejecutada
   // Opcional: Especificar el patrón para encontrar archivos de prueba
-  // testMatch: ['**/tests/**/*.test.js?(x)', '**/tests/**/*.spec.js?(x)'],
+  testMatch: ['**/tests/**/*.test.js'],
   // Opcional: Directorios a ignorar
   // testPathIgnorePatterns: ['/node_modules/'],
   // Opcional: Configuración de cobertura de código
